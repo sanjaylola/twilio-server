455d2f54450b29150f7d1ad7fc3a738349f15b4d
unknown <car.mailverify@gmail.com~>
2026-07-04 17:39:53 -0400

    Add /make-call endpoint to place outbound Twilio call

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const twilio = require('twilio');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const DESTINATION_NUMBER = '+16472027681';
+
+const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
 app.get('/', (req, res) => {
   res.send('Server is running');
+});
+
+app.post('/make-call', async (req, res) => {
+  try {
+    const call = await client.calls.create({
+      to: DESTINATION_NUMBER,
+      from: process.env.TWILIO_PHONE_NUMBER,
+      twiml: '<Response><Say>Hello, this is an automated call.</Say></Response>',
+    });
+    res.json({ success: true, callSid: call.sid });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, error: err.message });
+  }
 });
 
 app.listen(port, () => {
